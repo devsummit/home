9feed89e644d0ff24b145cb7a90e264020d87bd5
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-10-03 09:29:42 +0700

    Fetch communities data from server

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ function isMobile() {
 }
 
 window.addEventListener('scroll', function (e) {
-  if(!isMobile()) {
+  if (!isMobile()) {
     const body = document.querySelector('body');
     const nav = document.querySelector('#nav-main');
     if (body.scrollTop > 100) {
@@ -13,5 +13,26 @@ window.addEventListener('scroll', function (e) {
       nav.classList.remove('fixed-top');
       body.paddingTop = 0;
     }
+  }
+});
+
+new Vue({
+  el: '#app',
+
+  data: function () {
+    return {
+      communities: []
+    }
+  },
+
+  mounted: function () {
+    const self = this;
+    axios.get('http://api.devsummit.io:5000/api/v1/partners')
+      .then(function (response) {
+        self.communities = response.data.data;
+      })
+      .catch(function (err) {
+        console.error(err)
+      })
   }
 });
